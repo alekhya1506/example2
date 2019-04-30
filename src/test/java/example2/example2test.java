d7443e00c6da228a3eef43b94d50fb694dedd7c9
alekhya1506 <alekhya15k@gmail.com>
2019-04-30 23:54:58 +0530

    example2 commit

---
+++ src/test/java/example2/example2test.java
@@ -0,0 +1,14 @@
+package example2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class example2test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
